532eb35b57de3bf31621421a6552cc6bb8b6aeb3
Andrew <andrewsheu0617@gmail.com>
2024-01-16 16:44:39 +0800

    version-2

--- Script.js
+++ Script.js
@@ -39,10 +39,11 @@ month_picker.onclick = () => {
     month_list.classList.add('show')
 }
 
+localStorage.clear()
 
 // Year, Month get
 generateCalendar = (month, year) => {
-    localStorage.clear()
+    // localStorage.clear()
     let calendar_days = document.querySelector('#calendar-days')
     let calendar_year = document.querySelector('#year-display')
     let event_date = document.querySelector('#event-date')
@@ -79,6 +80,7 @@ generateCalendar = (month, year) => {
                 day.classList.add('curr_date') // add today date the class='curr'date'
             }
 
+
             day.addEventListener('click', function () {
                 // 清除之前選擇的日期的樣式
                 document.querySelectorAll('.selected-date').forEach(element => {
@@ -93,26 +95,26 @@ generateCalendar = (month, year) => {
                 let select_date = `${weekdays[first_day.getDay()]} ${i - first_day.getDay() + 1} ${months_name[month]} ${year}`
 
                 gobal_selectDate = select_date
-
                 event_date.innerHTML = select_date
+                EventList()
 
             });
         }
         calendar_days.appendChild(day)
     }
-    EventList()
 }
 
 
 
 function EventList(){
     // Create Event List
-    updateList()
     var text_input = document.querySelector('#text')
     var add_input = document.querySelector('#add')
-    var list_ul = document.querySelector('.eventlist')
+    var list_ul = document.querySelector('#eventlist')
 
     var dataBase = JSON.parse(localStorage.getItem(gobal_selectDate)) || []
+    var addtext = []
+
 
     text_input.addEventListener('keyup', function(event) {
         if (event.key === "Enter"){
@@ -122,11 +124,28 @@ function EventList(){
     add_input.addEventListener('click', addData)
     list_ul.addEventListener('click', deleteData)
 
+    updateList()
+
+
     function addData() {
+        if (localStorage.getItem(gobal_selectDate) !== null){
+            const previousList = JSON.parse(localStorage.getItem(gobal_selectDate))
+            console.log(previousList)
+            for (let i = 0; i < previousList.length; i++) {
+                var toaddprevious = {
+                    content: previousList[i].content
+                }
+                addtext.push(toaddprevious)
+                console.log(toaddprevious)
+            }
+        }
+        
+        console.log(addtext)
         var txt = text_input.value
 
         if (txt.trim() === ""){
-            alert('need to type some!')
+            console.log('alert')
+            //alert('need to type some!')
             return;
         }
 
@@ -134,13 +153,14 @@ function EventList(){
             content: txt
         }
 
-        dataBase.push(todo)
+        addtext.push(todo)
+        console.log(addData)
 
-        localStorage.setItem(gobal_selectDate, JSON.stringify(dataBase))
+        localStorage.setItem(gobal_selectDate, JSON.stringify(addtext))
+        console.log(addtext)
 
         text_input.value = ''
         updateList()
-
     }
 
     function deleteData(e){
@@ -152,46 +172,43 @@ function EventList(){
 
         localStorage.setItem(gobal_selectDate, JSON.stringify(dataBase));
         updateList();
-
     }
 
     function updateList(){
-        // let list = ''
-        // for (let i = 0; i < dataBase.length; i++) {
-        //     list += '<li><a href="#" data-num='+ i +'>Del </a>' + dataBase[i].content +'</li>'
-        // }
-        // list_ul.innerHTML = list
 
-        function isKeyInLocalStorage(gobal_selectDate) {
-            if( localStorage.getItem(gobal_selectDate) !== null ){
-                return false;
-            }
-            else{
-                
-            }
-        }
-        
-
-        for (let i = 0; i < localStorage.length; i++) {
-            if (gobal_selectDate === localStorage.key(i)){
-                const key = localStorage.key(i);
-                const value = localStorage.getItem(key);
-                const parsedValue = JSON.parse(value);
-                let list = ''
-
-                for (let i = 0; i < parsedValue.length; i++) {
-                    list += '<li><a href="#" data-num='+ i +'>Del </a>' + parsedValue[i].content +'</li>'
-                }
-                list_ul.innerHTML = list
-            }
-        }
-        // for (let i = 0; i < localStorage.length; i++) {
-        //     const key = localStorage.key(i);
-        //     const value = localStorage.getItem(key);
-        
-        //     console.log(`Key: ${key}, Value: ${value}`);
-        // }
+        console.log(gobal_selectDate)
+        if (isKeyInLocalStorage(gobal_selectDate)){}
     }
+
+    function isKeyInLocalStorage(key) {
+        let list = ''
+        console.log(key)
+        const storedData = localStorage.getItem(key)
+        console.log(storedData)
+        if(storedData === null ){
+            list_ul.innerHTML = '' // clear the list
+            return false;
+        }
+        else{
+            // console.log(storedData)
+            const value = JSON.parse(storedData)
+            console.log(value)
+
+            for (let i = 0; i < value.length; i++) {
+                list += '<li><a href="#" data-num='+ i +'>Del </a>' + value[i].content +'</li>'
+            }
+            console.log(list)
+            list_ul.innerHTML = list
+            return true;
+        }
+    }
+
+    // for (let i = 0; i < localStorage.length; i++) {
+    //     const key = localStorage.key(i);
+    //     const value = localStorage.getItem(key);
+    
+    //     console.log(`Key: ${key}, Value: ${value}`);
+    // }
 }
 
 let currDate = new Date()
